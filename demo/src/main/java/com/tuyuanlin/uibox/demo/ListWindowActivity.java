39f4880926eff68d360e62da8e4c0decffbe051d
tuyuanlin <tuyuanlin@xiaomi.com>
2019-02-20 10:31:38 +0800

    Initial commit

---
+++ demo/src/main/java/com/tuyuanlin/uibox/demo/ListWindowActivity.java
@@ -0,0 +1,17 @@
+package com.tuyuanlin.uibox.demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Copyright (C) 2013, Xiaomi Inc. All rights reserved.
+ */
+
+public class ListWindowActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_window);
+    }
+}
